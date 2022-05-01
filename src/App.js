8021280bb2f616c9f080841770f97cbaae69b91c
dano56ace <54921136+dano56ace@users.noreply.github.com>
2022-05-01 11:25:14 -0400

    added image tag

--- src/App.js
+++ src/App.js
@@ -301,18 +301,11 @@ function App() {
                     <CrossmintPayButton
                 collectionTitle="Rubber Doug - Genesis"
                 collectionDescription="The Rubber Doug's is a collection of 300 unique Doug NFTs— unique digital collectibles living on the Ethereum blockchain. Your Doug doubles as your Snap Delivered membership card, and grants access to members-only benefits. Future areas and perks can be unlocked by the community through roadmap activation"
-                collectionPhoto=""
+                collectionPhoto="https://rubberdoug.io/img/core-img/collection_cover.jpg"
                 clientId="e69d92ce-73fd-435a-ae2b-e4e7fa9da34e"
                 mintConfig={{"type":"erc-721","price":"0.11"}}
             />
                                             <s.SpacerSmall />
-                              <CrossmintPayButton
-                collectionTitle="Rubber Doug - Genesis"
-                collectionDescription="The Rubber Doug's is a collection of 300 unique Doug NFTs— unique digital collectibles living on the Ethereum blockchain. Your Doug doubles as your Snap Delivered membership card, and grants access to members-only benefits. Future areas and perks can be unlocked by the community through roadmap activation"
-                collectionPhoto=""
-                clientId="e69d92ce-73fd-435a-ae2b-e4e7fa9da34e"
-                mintConfig={{"type":"erc-721","price":"<SELECTED_PRICE_IN_ETHER>"}}
-            />
 
                                 <s.SpacerSmall />
 
